e3acfd5f190e5d7d2ce01de6174d699ae63f909c
Kartik Shridhar <kartiks@Kartiks-iMac.local>
2015-04-15 22:05:46 -0700

    HW1 completed

--- Spring2015/HW1.py
+++ Spring2015/HW1.py
@@ -1,5 +1,6 @@
 # Homework #1 ML 302
-from import numpy *
+# Kartik Shridhar
+from numpy import *
 import matplotlib.pyplot as plt
 
 
@@ -12,7 +13,7 @@ def max_cubed(x):
 
 def CubicSpline(w3, t):
     result = []
-    for item in numpy.nditer(t):
+    for item in nditer(t):
         value = w3[0] + w3[1] * item + w3[2] * item * item + \
                 w3[3] * item * item * item + \
                 w3[4] * max_cubed(item+2) + w3[5] * max_cubed(item) + \
@@ -21,44 +22,100 @@ def CubicSpline(w3, t):
 
     return result
 
-# training set "/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample1_train.txt"
 
-
-def LinearRegressoin(input_file):
-    data = numpy.loadtxt(input_file)
+def LinearRegression(input_file):
+    data = loadtxt(input_file)
     X = [a[0] for a in data]
     Y = [a[1] for a in data]
     # Matrix
     A = [[1, a] for a in X]
-    weights = numpy.linalg.lstsq(A, Y)[0]
+    weights = linalg.lstsq(A, Y)[0]
     return weights
-   
-   
-   def MeanSquaredError(w):
-    testdata = numpy.loadtxt("/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample1_test.txt")
-    X = [a[0] for a in testdata]
-    Y = [a[1] for a in testdata]
-    Y1 = [w[0]+w[1]*a for a in X]
-    
 
+
+def LinearMSE(w, test_file):
+    testdata = loadtxt(test_file)
+    X = [a[0] for a in testdata]
+    Y = array([a[1] for a in testdata])
+    Y1 = array([w[0]+w[1]*a for a in X])
+    mse = mean(square(Y1-Y))
+    return mse
+
+
+def CubicRegression(input_file):
+    data = loadtxt(input_file)
+    X = [a[0] for a in data]
+    Y = [a[1] for a in data]
+    # Matrix
+    A =  [[1,a, a*a, a*a*a] for a in X]
+    weights = linalg.lstsq(A, Y)[0]
+    return weights
+
+
+def CubicMSE(w,test_file):
+    testdata = loadtxt(test_file)
+    X = [a[0] for a in testdata]
+    Y = array([a[1] for a in testdata])
+    Y1 = array([w[0]+w[1]*t+w[2]*t*t+w[3]*t*t*t for t in X])
+    mse = mean(square(Y1-Y))
+    return mse
+
+
+def CubicSplineRegression(input_file):
+    data = loadtxt(input_file)
+    X = [a[0] for a in data]
+    Y = [a[1] for a in data]
+    # Matrix
+    A =  [[1, a, a*a, a*a*a, max_cubed(a+2), max_cubed(a), max_cubed(a-2)] for a in X]
+    weights = linalg.lstsq(A, Y)[0]
+    return weights
+
+
+def CubicSplineMSE(w,test_file):
+    testdata = loadtxt(test_file)
+    X = [a[0] for a in testdata]
+    Y = array([a[1] for a in testdata])
+    Y1 = array(CubicSpline(w,array(X)))
+    mse = mean(square(Y1-Y))
+    return mse
+    
+    
 ###############################################################################
 #
 # This is the main() entrypoint of every top-level Python program.
 #
 # if __name__ == '__main__':
 #
-#  data = numpy.loadtxt("/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample1_train.txt")
-#  X = [a[0] for a in data]
-#  Y = [a[1] for a in data]
-#
-#  #Spline Cube
-#  X7 = [[1,a,a*a,a*a*a,max_cubed(a+2),max_cubed(a),max_cubed(a-2)] for a in X]
-#  w3,_,_,_ = numpy.linalg.lstsq(X7,Y)
-#  t = numpy.arange(-4,4,0.1)
-#
-# spline_values = compute_Y(w3,t)
-#
-# dummy=1
+
+sample1_train = "/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample1_train.txt"
+sample1_test = "/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample1_test.txt"
+sample2_train = "/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample2_train.txt"
+sample2_test = "/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample2_test.txt"
+
+weights = LinearRegression(sample1_train)
+mse = LinearMSE(weights, sample1_test)
+print "MSE for sample1 using Linear regression = %s" % mse
+
+weights = LinearRegression(sample2_train)
+mse = LinearMSE(weights,sample2_test)
+print "MSE for sample2 using Linear regression = %s" % mse
+
+weights = CubicRegression(sample1_train)
+mse = CubicMSE(weights, sample1_test)
+print "MSE for sample1 using cubic regression = %s" % mse
+
+weights = CubicRegression(sample2_train)
+mse = CubicMSE(weights,sample2_test)
+print "MSE for sample2 using cubic regression = %s" % mse
+
+
+weights = CubicSplineRegression(sample1_train)
+mse = CubicSplineMSE(weights, sample1_test)
+print "MSE for sample1 using cubic spline regression = %s" % mse
+
+weights = CubicSplineRegression(sample2_train)
+mse = CubicSplineMSE(weights, sample2_test)
+print "MSE for sample2 using cubic spline regression = %s" % mse
 
 ###############################################################################
 ###############################################################################
