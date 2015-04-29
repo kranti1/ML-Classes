aa58e671159be5aea9953347921832af49ba99e7
Kartik Shridhar <kartiks@Kartiks-iMac.local>
2015-04-28 22:48:34 -0700

    HW2 final

--- Spring2015/HW2.py
+++ Spring2015/HW2.py
@@ -21,9 +21,6 @@ def cub_spl_basis(t):
     return[1, t, t**2, t**3, spl(t, 2), spl(t, 0), spl(t, -2)]
 
 
-# r is a list of the r terms
-
-
 def nat_spl(t, r, i):
     return ((spl(t, r[i]) - spl(t, r[i+1]))/(r[i+1]-r[i]))
 
@@ -68,33 +65,51 @@ sample1_test = "/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw
 sample2_train = "/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample2_train.txt"
 sample2_test = "/Users/kartiks/Documents/github/ml/Spring2015/HW1_sample_data/hw1_sample2_test.txt"
 
-data = np.loadtxt(sample2_test)
-#r = [-4,-3,-2,-1,1,2,3, 4]
-r = [-4, 4]
-r = [-4, 4]
-r = [-4, 4]
-r = [-4, 4]
-r = [-4, 4]
-r = [-4, 4]
-r = [-4, 4]
-#r = [-4,-3, -2, -1, 1, 2, 3, 4]
+data = np.loadtxt(sample1_test)
 
 
-errors = []
-for training, validation in k_fold(data, 5):
-    X = [a[0] for a in training]
-    Y = [a[1] for a in training]
-    w1 = np.linalg.lstsq([natural_cub_spl_basis(x, r) for x in X], Y)[0]
-    error = mse_nat_cub_spline(w1, natural_cub_spl_basis, r, training)
-    errors.append(error)
-    print "MSE = %s" % error
-mean_error = np.mean(errors)
-print "Mean of the errors = %s" % mean_error
+for i in range (2,9,1):
+    print "\nNumber of Knots k = %s"  % i
+    r = np.linspace(-4,4,i)
+    errors = []
+    
+    for training, validation in k_fold(data, 5):
+        # TRAINING SET
+        X = [a[0] for a in training]
+        Y = [a[1] for a in training]
+        w1 = np.linalg.lstsq([natural_cub_spl_basis(x, r) for x in X], Y)[0]
+        error = mse_nat_cub_spline(w1, natural_cub_spl_basis, r, training)
+        errors.append(error)
+        print "MSE = %s" % error
+        
+    mean_error = np.mean(errors)   
+    print "Mean of the errors = %s" % mean_error
+    
+    # VALIDATION SET
+    Xf = [a[0] for a in validation]
+    Yf = [a[1] for a in validation]
+    omega = w1 = np.linalg.lstsq([natural_cub_spl_basis(x, r) for x in Xf], Yf)[0]
+    error = mse_nat_cub_spline(omega, natural_cub_spl_basis, r, validation)
+    print "MSE for Validation set = %s" % error
 
-Xf = [a[0] for a in data]
-Yf = [a[1] for a in data]
-omega = w1 = np.linalg.lstsq([natural_cub_spl_basis(x, r) for x in Xf], Yf)[0]
-t = np.arange(-4, 4, 0.1)
-Y1 = [(sum(omega* natural_cub_spl_basis(x, r))) for x in t]
-plt.plot(Xf, Yf, 'ro', t, Y1)
-plt.show()
+    # PLOT VALIDATION SET
+    t = np.arange(-4, 4, 0.1)
+    Y1 = [(sum(omega* natural_cub_spl_basis(x, r))) for x in t]
+    plt.plot(Xf, Yf, 'ro', t, Y1)
+    plt.show()
+    
+    # TRAIN WITH ALL DATA
+    Xf = [a[0] for a in data]
+    Yf = [a[1] for a in data]
+    omega = w1 = np.linalg.lstsq([natural_cub_spl_basis(x, r) for x in Xf], Yf)[0]
+    error = mse_nat_cub_spline(omega, natural_cub_spl_basis, r, data)
+    print "MSE for all training data = %s \n" % error
+    
+    # PLOT ALL DATA SET
+    t = np.arange(-4, 4, 0.1)
+    Y1 = [(sum(omega* natural_cub_spl_basis(x, r))) for x in t]
+    plt.plot(Xf, Yf, 'ro', t, Y1)
+    plt.show()    
+    
+    
+    print "###################################################################"
